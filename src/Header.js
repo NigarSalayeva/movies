bc8f2d1cd76d07a6282bb17044496d47a8d7aa59
NigarSalayeva <78052772+NigarSalayeva@users.noreply.github.com>
2023-01-03 02:59:34 +0400

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,19 @@ import './Header.css';
 function Header() {
     return (
     <div id='header'>
-      <h2>Need For Films</h2>
+      <nav class="sticky">
+  {/* <input type="checkbox" id="check"> */}
+  {/* /<label for="check" class="checkbtn">
+    <i class="fas fa-bars"></i>
+  </label>  */}
+  <label class="logo">Home Page</label>
+  <ul class="a">
+    <li><a class="n" href="App.js" target="_blank">Movies page</a></li>
+    <li><a class="n" href="https://github.com/NigarSalayeva" target="_blank">Git-Hub Account</a></li>
+    <li><a class="n" href="https://nigarsalayeva.github.io/nsalayeva.github.io/" target="_blank">Git-Hub Page</a></li>
+
+  </ul>
+</nav>
     </div>
     );
   }
