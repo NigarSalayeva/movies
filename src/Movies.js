8e29c03314495cd3adc4934be425543cfec05956
NigarSalayeva <78052772+NigarSalayeva@users.noreply.github.com>
2023-01-03 01:30:33 +0400

    Update Movies.js

--- src/Movies.js
+++ src/Movies.js
@@ -6,15 +6,15 @@ function Movies(props) {
     console.log(location,"location")
     const item = location.state?.item
   return(
-    <div>
-        <img src={item.Poster_Link} alt='help'/>
+    <div class="lock2">
+        <img  src={item.Poster_Link} alt='help'/>
         <p class="ser">{item.Series_Title+' ('+item.Released_Year+')'}</p>
               <p class="ser">IMDb Rating: {item.IMDB_Rating}</p>
-              <p class="dir">Overview: {item.Overview}</p>
-              <p class="dir">Genre: {item.Genre}</p>
-              <p class="dir">Runtime: {item.Runtime}</p>
-              <p class="dir">Director: {item.Director}</p>
-              <p class="desc">{item.Star1+', '+item.Star2+', '+item.Star3}</p>
+              <p class="ser">Genre: {item.Genre}</p>
+              <p class="dir2">Overview:</p><p class="dir"> {item.Overview}</p>
+              <p class="dir"><b>Runtime:</b> {item.Runtime}</p>
+              <p class="dir"><b>Director:</b> {item.Director}</p>
+              <p class="desc"><b>Stars: </b>{item.Star1+', '+item.Star2+', '+item.Star3+', '+item.Star4}</p>
     </div>
   )
   }  
