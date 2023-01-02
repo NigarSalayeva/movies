0ef59512f62339789d824e045975e7d1d28614b9
NigarSalayeva <78052772+NigarSalayeva@users.noreply.github.com>
2023-01-03 01:47:26 +0400

    Update Movies.js

--- src/Movies.js
+++ src/Movies.js
@@ -9,10 +9,10 @@ function Movies(props) {
     <div class="lock2">
         <img  src={item.Poster_Link} alt='help'/>
         <p class="ser">{item.Series_Title+' ('+item.Released_Year+')'}</p>
-              <p class="ser">IMDb Rating: {item.IMDB_Rating}</p>
+              <p class="ser">IMDb Rating: ⭐ {item.IMDB_Rating} </p>
               <p class="ser">Genre: {item.Genre}</p>
               <p class="dir2">Overview:</p><p class="dir"> {item.Overview}</p>
-              <p class="dir"><b>Runtime:</b> {item.Runtime}</p>
+              <p class="dir"><b>Runtime: 🕑 </b> {item.Runtime}</p>
               <p class="dir"><b>Director:</b> {item.Director}</p>
               <p class="desc"><b>Stars: </b>{item.Star1+', '+item.Star2+', '+item.Star3+', '+item.Star4}</p>
     </div>
