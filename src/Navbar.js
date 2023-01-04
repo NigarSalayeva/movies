7cd6c77f0a63680214474640580ab3c6e8622a51
NigarSalayeva <78052772+NigarSalayeva@users.noreply.github.com>
2023-01-04 19:16:40 +0400

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,6 +5,7 @@ import "./Navbar.css";
 export default function Navbar() {
   return (
     <nav className="nav">
+      
       <NavLink to="/">Home</NavLink>
       <NavLink to="/Movies">Movies</NavLink>
       <NavLink to="/Contact">Contact</NavLink>
