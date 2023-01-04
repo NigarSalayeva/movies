0c1a0bcff3d93b24972843db6ec3f73dffa476ed
NigarSalayeva <78052772+NigarSalayeva@users.noreply.github.com>
2023-01-04 19:12:34 +0400

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,19 @@
+import { getMouseEventOptions } from '@testing-library/user-event/dist/utils';
+
 import './Header.css';
+
+import Movies from './Movies.js';
 function Header() {
     return (
     <div id='header'>
-      <nav class="sticky">
+      <nav>
   {/* <input type="checkbox" id="check"> */}
   {/* /<label for="check" class="checkbtn">
     <i class="fas fa-bars"></i>
   </label>  */}
   <label class="logo">Home Page</label>
   <ul class="a">
-    <li><a class="n" href="App.js" target="_blank">Movies page</a></li>
+    <li><a class="n" href={Movies.js} target="_blank">Movies page</a></li>
     <li><a class="n" href="https://github.com/NigarSalayeva" target="_blank">Git-Hub Account</a></li>
     <li><a class="n" href="https://nigarsalayeva.github.io/nsalayeva.github.io/" target="_blank">Git-Hub Page</a></li>
 
