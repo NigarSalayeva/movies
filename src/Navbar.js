757c89e8d197fd1cdb9dc58d41f4564fba3e413a
NigarSalayeva <78052772+NigarSalayeva@users.noreply.github.com>
2023-01-04 19:12:41 +0400

    Create Navbar.js

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/Movies">Movies</NavLink>
+      <NavLink to="/Contact">Contact</NavLink>
+    </nav>
+  );
+}
