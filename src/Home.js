fac96a84d98c8f2a0e20d46f03a86bc88efcfbeb
NigarSalayeva <78052772+NigarSalayeva@users.noreply.github.com>
2023-01-04 21:02:00 +0400

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,51 @@
+import "./Home.css";
 export default function Home() {
-    return <div>This is the Home</div>;
+    return <div>
+<div class="team">
+    <h3>Our team</h3>
+    <p>Welcome to our website, we hope you enjoy while browsing our website. First, let's introduce our group and members: Nigar Salayeva, Laman Khudadatzade, Surkhay Fatullayev. It's our first project as a group and the project is about movies. When scrolling down first you will get information about our group members and then about our projects. Have fun!
+</p>
+<h3>Team Members</h3>
+<div class="member">
+    <div class="nude">
+      <h4>Nigar Salayeva</h4>
+      <h5>Student from ADA University</h5>
+      <p>Nigar Salayeva is one of the creators of the webpage. She is a student from SITE(School of Information Technology and Engineering) faculty. She is here with us to show that women can also be ambitious in specialties such as computer science and engineering.</p>
+    </div>
+    <div class="blue">
+      <h4>Surkhay Fatullayev</h4>
+      <h5>Student from ADA University</h5>
+      <p>Surkhay Fatullayev is another creator of the webpage. He is also a student from SITE(School of Information Technology and Engineering) faculty. His dream is to  specialize in this field and be known all over the world as the best web developer.</p>
+    </div>
+  <div class="green">
+     <h4>Laman Khudadatzade</h4>
+     <h5>Student from ADA University</h5>
+     <p>Laman Khudadatzade is one more creator of the website.She is a student of a SITE faculty and believes that computers are taking over the world, so let's develop softwares carefully and gracefully!</p>
+  </div>
+      </div>
+      
+<ul>
+    <li>
+      <h2>Website Project</h2>
+      <p> Our first project is to create our own website and digital-card. We had to create the website using all the CSS we had learned. Also, we had to add  a link to our Github account, a  link to our Github page, and a link to the personal digital card in our navigation bar. Links are placed under each member's information box.</p>
+    </li>
+    <li>
+      <h2>Weather App</h2>
+      <p></p>
+
+    </li>
+    <li>
+      <h2>Movies Project</h2>
+      <p> Movies project is our last project which we currently present to you. We are so excited about this project and it will be great work for us!</p>
+    </li>
+  </ul>
+
+  
+  
+  
+
+  
+  </div>
+
+    </div>;
   }
