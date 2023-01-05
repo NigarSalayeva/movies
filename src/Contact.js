9be02710171a124dd285de19efe214f7b15d1538
NigarSalayeva <78052772+NigarSalayeva@users.noreply.github.com>
2023-01-06 00:51:31 +0400

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -18,21 +18,20 @@ const Contact = () => {
                 method: "POST",
                 headers: { 'content-type': 'application/json' },
                 body: JSON.stringify(data)
-            }).then((res) => {
-                toast.success('Mail Sent successfully.')
-    
-            }).catch((err) => {
-                toast.error('Failed :' + err.message);
             });
-        
+            subjectchange('');
+    namechange('');
+    emailchange('');
+    phonechange('');
+    messagechange('');
     }
     return (
-        <div>
-            <div className="offset-lg-3 col-lg-6">
-                <form className="container" onSubmit={handlesubmit}>
-                    <div className="card">
-                        <div className="card-header">
-                            <h1>Contact Page</h1>
+        <div >
+            <div  className="offset-lg-3 col-lg-6">
+                <form style={{marginTop:"15px"}} className="container" onSubmit={handlesubmit}>
+                    <div style={{backgroundColor:"#F8F4EA", borderRadius:"15px", boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px"}}className="card">
+                        <div style={{backgroundColor:"#3C2317", borderRadius:"10px"}} className="card-header">
+                            <h1 id="center" style={{color: "white"}}><b>Contact Us</b></h1>
                         </div>
                         <div className="card-body">
 
@@ -40,32 +39,32 @@ const Contact = () => {
                 
                                 <div className="col-lg-6">
                                     <div className="form-group">
-                                        <label>Subject <span className="errmsg">*</span></label>
+                                        <label><b>Subject</b></label>
                                         <input value={subject} onChange={e => subjectchange(e.target.value)} type="subject" className="form-control"placeholder="Subject"></input>
                                     </div>
                                 </div>
                                 <div className="col-lg-6">
                                     <div className="form-group">
-                                        <label>Full Name <span className="errmsg">*</span></label>
+                                        <label><b>Full Name</b> </label>
                                         <input value={name} onChange={e => namechange(e.target.value)} className="form-control" placeholder="Your Name"></input>
                                     </div>
                                 </div>
                                 <div className="col-lg-6">
                                     <div className="form-group">
-                                        <label>Email <span className="errmsg">*</span></label>
+                                        <label><b>Email</b></label>
                                         <input value={email} onChange={e => emailchange(e.target.value)} className="form-control" placeholder="sample@gmail.com"></input>
                                     </div>
                                 </div>
                                 <div className="col-lg-6">
                                     <div className="form-group">
-                                        <label>Phone <span className="errmsg"></span></label>
+                                        <label><b>Phone </b></label>
                                         <input value={phone} onChange={e => phonechange(e.target.value)} className="form-control" placeholder="+994-XX-XXX-XX-XX"></input>
                                     </div>
                                 </div>
                             
                                 <div className="col-lg-12">
                                     <div className="form-group">
-                                        <label>Message</label>
+                                        <label><b>Message</b></label>
                                         <textarea value={message} onChange={e => messagechange(e.target.value)} className="form-control" placeholder="Type here..."></textarea>
                                     </div>
                                 </div>
@@ -75,7 +74,7 @@ const Contact = () => {
 
                         </div>
                         <div className="card-footer">
-                            <button id="center" type="submit" className="btn btn-primary">Send message!</button> 
+                            <button style={{backgroundColor:"black", color:"white", width:"100%"}}  type="submit" className="btn btn-primary">Send message!</button> 
                            
                         </div>
                     </div>
